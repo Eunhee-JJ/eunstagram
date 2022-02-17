5a054263f2a85bfec533cedfde0f600bca08dd39
Eunhee-JJ <heestory0324@gmail.com>
2022-02-17 22:38:46 +0900

    [FEAT] progressbar GONE 처리, 프로필 옵션에서 뒤로가기 구현

--- app/src/main/java/com/example/eunstagram/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/eunstagram/Profile/ProfileActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,14 +22,17 @@ public class ProfileActivity extends AppCompatActivity { // 안드로이드 3.0 
     private static final String TAG = "ProfileActivity";
     private static final int ACTIVITY_NUM = 4;
 
-
     private Context mContext = ProfileActivity.this;
+
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: stated.");
+        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
+        mProgressBar.setVisibility(View.GONE);
 
         setupBottomNavigationView();
         setupToolbar();
